557a249eb127b2067172a8aa4b26aacaa9578e1c
Nick Bottomley <nhbottomley@gmail.com>
2014-06-21 16:07:31 -0700

    Initial.

---
+++ spec/func.js
@@ -0,0 +1,20 @@
+/* global func, describe, it, expect, should */
+
+describe('func()', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(func).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
